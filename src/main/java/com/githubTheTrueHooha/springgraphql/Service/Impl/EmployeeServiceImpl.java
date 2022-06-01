0f425de70ef72d0973b540b7ef2ac321b9b5e213
David Ogar <owogogahhero@outlook.com>
2022-06-01 13:07:09 +0100

    feat: created employee service implementation

---
+++ src/main/java/com/githubTheTrueHooha/springgraphql/Service/Impl/EmployeeServiceImpl.java
@@ -0,0 +1,64 @@
+package com.githubTheTrueHooha.springgraphql.Service.Impl;
+
+import com.githubTheTrueHooha.springgraphql.Model.Employees;
+import com.githubTheTrueHooha.springgraphql.Repository.EmployeeRepository;
+import com.githubTheTrueHooha.springgraphql.Service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private final EmployeeRepository employeeRepository;
+
+    @Override
+    public Employees addNewEmployee(Employees employees) {
+        return employeeRepository.save(employees);
+    }
+
+    @Override
+    public List<Employees> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employees getEmployeeById(Long id) throws RuntimeException {
+        Optional<Employees> employees = employeeRepository.findById(id);
+        if (employees.isPresent()){
+            return employees.get();
+        } else {
+            throw new RuntimeException(HttpStatus.NOT_FOUND.toString());
+        }
+    }
+
+    @Override
+    public Employees editEmployeeData(Employees employees, Long id) {
+        Employees currentEmployees = employeeRepository.findById(id)
+                .orElseThrow( () -> new RuntimeException(HttpStatus.NOT_FOUND.toString()));
+        currentEmployees.setFirstName(employees.getFirstName());
+        currentEmployees.setLastName(employees.getLastName());
+        currentEmployees.setEmail(employees.getEmail());
+        currentEmployees.setHomeAddress(employees.getHomeAddress());
+        currentEmployees.setPhoneNumber(employees.getPhoneNumber());
+
+        employeeRepository.save(currentEmployees);
+        return currentEmployees;
+    }
+
+    @Override
+    public void deleteEmployee(Long id) throws RuntimeException {
+        //check if the employee data exists in the database and deletes by ID
+        if (employeeRepository.existsById(id)){
+            employeeRepository.deleteById(id);
+        } else {
+            throw new RuntimeException(HttpStatus.NOT_FOUND.toString());
+        }
+    }
+}
