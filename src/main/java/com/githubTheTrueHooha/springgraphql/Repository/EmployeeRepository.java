0fe51763dfbc2eda78f10a8c1b1023767aa0eb46
David Ogar <owogogahhero@outlook.com>
2022-06-02 15:01:12 +0100

    no changes made

--- src/main/java/com/githubTheTrueHooha/springgraphql/Repository/EmployeeRepository.java
+++ src/main/java/com/githubTheTrueHooha/springgraphql/Repository/EmployeeRepository.java
@@ -3,6 +3,8 @@ package com.githubTheTrueHooha.springgraphql.Repository;
 import com.githubTheTrueHooha.springgraphql.Model.Employees;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employees, Long> {
+public interface EmployeeRepository extends JpaRepository<Employees, Integer> {
+
+
 
 }
